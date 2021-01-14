1f998e4ea8b8875246cb8daea8bb344e4158137e
delaney-lekien <delaneylekien@gmail.com>
2021-01-14 17:01:28 -0500

    First commit, reading from CSV and menu working

---
+++ src/main/scala/example/Book.scala
@@ -0,0 +1,9 @@
+package example
+
+import scala.io.Source
+
+object Book extends App {
+  val cli = new Cli()
+  cli.menu()
+    
+}
---
+++ src/main/scala/example/FileUtil.scala
@@ -0,0 +1,16 @@
+package example
+
+import scala.io.BufferedSource
+import scala.io.Source
+
+object ReadCSV {
+
+  def get() : Unit = {
+    println("Title, Author, Price, ISBN")
+    val bufferedSource = io.Source.fromFile("BookLog.csv")
+    for (line <- bufferedSource.getLines) {
+        var Array(title, author, price, isbn) = line.split(",").map(_.trim)
+        println(s"""|$title $author $price $isbn""".stripMargin)
+  }
+ }
+}
